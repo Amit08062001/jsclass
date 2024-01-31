bdc18dabcca5f4f5151df777b4691f376c5d4f9b
Amit Kumar Singh <157602171+Amit08062001@users.noreply.github.com>
2024-01-31 08:04:09 +0000

    minor changes

--- basic.js
+++ basic.js
@@ -1,6 +1,9 @@
 let userId=4658;
 let userName="Amit06";
 let userCity="Siwan";
+const teamName = "kunal";
 console.log(userId)
 console.log([userName, userId, userCity]);
 console.table([userName, userCity])
+
+console.log(teamName)
