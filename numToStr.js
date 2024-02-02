d726643b9c311dca6ecd11b15c667e0af9681928
Amit Kumar Singh <157602171+Amit08062001@users.noreply.github.com>
2024-02-02 08:35:06 +0000

    changing

---
+++ numToStr.js
@@ -0,0 +1,61 @@
+let name= 'Amit';
+ let age = 23;
+let city='siwan';
+ let isNumber = false;
+ let contact = null;
+ let about= 
+//console.log(typeof 'Amit' );
+//console.log(typeof  age );
+//console.log(typeof city );
+//console.log(typeof isNumber);
+//console.log(typeof contact);
+//console.log(typeof about);
+ checknum = Number(name);
+//console.log(typeof checknum);
+//console.log(checknum);
+
+checkbool = Number(isNumber);
+//console.log(checkbool);
+
+checknull= Number(contact);
+//console.log(typeof checknull);
+//console.log(checknull)
+  
+
+connum=String(age);
+//console.log(typeof conStr);
+//console.log(conStr);
+
+
+conbool=String(isNumber);
+//console.log(typeof isNumber);
+
+connull = String(contact);
+//console.log(typeof connull);
+/*
+let amit = ['Amit','siwan'];
+console.log(amit);
+console.log(typeof amit)
+
+
+amitObj={name: 'amit',
+city: 'siwan',
+state : 'bihar'
+
+}
+console.log(amitObj);
+console.log(typeof amitObj)
+
+*/
+
+add=function(){
+    console.log(a+b+2);
+}
+let a=10;
+let b=12;
+console.log(add(a,b));
+
+mul=function(){
+    console.log(a*b+2);
+}
+console.log(mul(a,b));
