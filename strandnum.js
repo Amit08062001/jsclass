7113ccd9b85f9b0785cfbda392d0be28c4f23f20
Amit Kumar Singh <157602171+Amit08062001@users.noreply.github.com>
2024-02-06 10:37:27 +0000

    mjor

---
+++ strandnum.js
@@ -0,0 +1,54 @@
+////Strings////
+
+let name = "amit";
+let age = 18;
+//console.log( "my name is" + name +" and my age is" + age);  /// concatination
+ 
+//console.log(`I am ${name} and my age is ${age}.`); /// string interpolation
+
+//console.log(`I am ${name.toUpperCase()} and my age is ${age}.`); /// string interpolation
+
+let myname="Amit Kumar Singh"
+let myage = 23;
+let address = "bihar";
+//console.log(`my name is ${myname} and my age is ${myage} and I am from ${address}`);
+/*
+console.log(myname.toUpperCase())
+console.log(myname.toLowerCase());
+console.log(myname.indexOf('a'));
+console.log(myname.indexOf('m'));
+console.log(myname.lastIndexOf('m'));
+console.log(Number(myage));
+console.log(typeof 'myage');
+console.log(myname.charAt(6));
+console.log(myname.trim());
+console.log(myname.slice(2,7));
+console.log(myname.split());
+console.log(myname.replace('Amit','Kunal'));
+console.log(myname.replace(/Amit/g,'kunal'));
+console.log(myname.repeat(4));
+console.log(myname.at(-2));
+console.log(myname.search("Singh"));
+console.log(myname.padStart(59,"good Morning"))
+console.log(myname.includes('Singh'));
+console.log(myname.startsWith('g',14));
+console.log(myname.indexOf('g'));
+*/
+
+/////Number////
+let number = new Number(100000000);
+console.log(number);
+console.log(number.toFixed(2));
+let anonum= 234.45
+console.log(anonum.toPrecision(4));
+console.log(Math.abs(-500));
+console.log(Math.round(54.6658));
+console.log(Math.ceil(45.1));
+console.log(Math.floor(56.67));
+console.log(Math.min(4,76,8,7,68,8,5));
+console.log(Math.floor(Math.random()*100+1));
+
+
+
+
+
