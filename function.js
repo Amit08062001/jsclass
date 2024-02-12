6a01c7272531094407a029b3a12bda7921634fa6
Amit Kumar Singh <157602171+Amit08062001@users.noreply.github.com>
2024-02-12 11:03:28 +0000

    major changes

--- function.js
+++ function.js
@@ -2,4 +2,67 @@
    console.log( "hey" + "morning" )
     
 }
-hello()
+//hello()
+//////////
+
+function addTwo(num1,num2){
+  console.log(num1+num2);
+}
+//addTwo(34,67);
+
+////////////////
+
+function addTwoSub(num1,num2){
+  let sub=num1-num2;
+  
+  return sub;
+}
+//console.log(addTwoSub(56,45));
+
+//////
+function logInUser(Username){
+  if(Username==undefined){
+    console.log("provide valid username")
+    return;
+  } 
+
+  return `${Username} has loggedIn`;
+}
+
+ //console.log(logInUser());
+
+/////////
+let sum =0;
+function addValue(...num1){
+  for(let i=0;i<num1.length;i++){
+  sum = sum+num1[i];
+  }
+ return sum;
+}
+//addValue(300,400,5000)
+//console.log(sum);
+
+//////////////
+
+function changeName(theObject){
+  theObject.name="kunal";
+ }
+
+ let myProfile={
+  name:"amit",
+  pin: 841406,
+  state:"Bihar",
+ };
+ 
+ console.log(myProfile.name);
+ changeName(myProfile);
+ console.log(myProfile.name);
+ 
+ function changeArr(arr){
+  arr[0]= 40;
+ }
+
+ let arr= [300,500,600];
+ console.log(arr);
+ changeArr(arr);
+ console.log(arr);
